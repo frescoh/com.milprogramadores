fb23a4c916442cb03da72257dba9978e51b85977
Hernan Fresco <herny.12@gmail.com>
2022-04-17 02:16:19 -0300

    Mejora en equals

--- Modulo2TP2/Contacto.java
+++ Modulo2TP2/Contacto.java
@@ -42,15 +42,20 @@ public class Contacto {
 		return "Nombre: "+this.nombre+"\nNumero: "+this.numero;
 	}
 	
-	 @Override
-    public boolean equals(Object o) {
-		 return this.compareTo((Contacto)o)==0;
-	 }
 	 
 	 @Override
     public int hashCode() {
         return Objects.hash(nombre);
     }
+	
+	 @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Contacto contacto= (Contacto) o;
+        return this.compareTo((Contacto)o)==0;
+    }
+
 	
 
 }
